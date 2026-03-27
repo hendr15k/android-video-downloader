a21a1c3bdaae45a9addb66804b970ece9daa7dc6
Hendrik Schmitz <hendrik.schmitz03.sh@gmail.com>
2026-03-27 23:56:09 +0800

    fix: Add hilt-work dependency for HiltWorker support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
 
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
